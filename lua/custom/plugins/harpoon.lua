87827bee04c8b06a6912cc68fee28c84b72c3d46
blackzarifa <rafaelirades@gmail.com>
2024-11-21 23:32:04 -0300

    fix: deprecated function

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
 
     -- Command-line interface configuration
     vim.keymap.set('n', '<leader>a', function()
-      harpoon:list():append()
+      harpoon:list():add()
     end, { desc = 'Add file to Harpoon' })
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
