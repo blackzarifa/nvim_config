363660361868387d58cf99757c1642530cc94738
blackzarifa <rafaelirades@gmail.com>
2024-11-23 23:39:14 -0300

    refac: toggle quickfix list

--- init.lua
+++ init.lua
@@ -110,7 +110,13 @@ vim.keymap.set('i', '<C-b>', '<C-o>dvb')
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>q', function()
+  if vim.fn.getloclist(0, { winid = 0 }).winid == 0 then
+    vim.diagnostic.setloclist()
+  else
+    vim.cmd.lclose()
+  end
+end, { desc = 'Toggle diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
