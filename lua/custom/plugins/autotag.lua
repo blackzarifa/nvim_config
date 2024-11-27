832a131e53cee9efb8c8ac29d648dce88fcfbc14
Rafael <rafaelirades@gmail.com>
2024-11-27 09:10:16 -0300

    refac: configs autotag

--- lua/custom/plugins/autotag.lua
+++ lua/custom/plugins/autotag.lua
@@ -3,29 +3,6 @@ return {
   event = 'InsertEnter',
   dependencies = 'nvim-treesitter/nvim-treesitter',
   config = function()
-    require('nvim-ts-autotag').setup {
-      filetypes = { 'html', 'xml', 'svelte', 'vue' },
-      enable_rename = true,
-      skip_tags = {
-        'area',
-        'base',
-        'br',
-        'col',
-        'command',
-        'embed',
-        'hr',
-        'img',
-        'slot',
-        'input',
-        'keygen',
-        'link',
-        'meta',
-        'param',
-        'source',
-        'track',
-        'wbr',
-        'menuitem',
-      },
-    }
+    require('nvim-ts-autotag').setup {}
   end,
 }
