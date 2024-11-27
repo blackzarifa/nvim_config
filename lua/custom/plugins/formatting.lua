25a6d20448f0e8c0c809afdd85f14703b3f5c4a5
Rafael <rafaelirades@gmail.com>
2024-11-27 10:06:51 -0300

    update: syntax

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -15,24 +15,26 @@ return {
     },
     opts = {
       formatters_by_ft = {
-        -- Use a sub-list to run only the first available formatter
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-        vue = { { 'prettierd', 'prettier' } },
-        css = { { 'prettierd', 'prettier' } },
-        html = { { 'prettierd', 'prettier' } },
-        json = { { 'prettierd', 'prettier' } },
-        yaml = { { 'prettierd', 'prettier' } },
-        markdown = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettierd', 'prettier' },
+        typescript = { 'prettierd', 'prettier' },
+        vue = { 'prettierd', 'prettier' },
+        css = { 'prettierd', 'prettier' },
+        html = { 'prettierd', 'prettier' },
+        json = { 'prettierd', 'prettier' },
+        yaml = { 'prettierd', 'prettier' },
+        markdown = { 'prettierd', 'prettier' },
         lua = { 'stylua' },
         python = { 'black' },
       },
-      -- Set up format-on-save
+      -- Add this to configure formatter fallback behavior
       format_on_save = {
-        -- These options will be passed to conform.format()
         timeout_ms = 500,
         lsp_fallback = true,
       },
+      -- Add this to control if formatters should run in parallel
+      parallel_workers = 1,
+      -- Add this to stop after the first successful formatter
+      stop_after_first = true,
     },
   },
 }
