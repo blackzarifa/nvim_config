99377bd2c74bf6f151a3cb89281a883cd4f9fd95
blackzarifa <rafaelirades@gmail.com>
2024-12-01 17:04:49 -0300

    add: themery

---
+++ lua/custom/plugins/themery.lua
@@ -0,0 +1,52 @@
+return {
+  'zaldih/themery.nvim',
+  lazy = false,
+  config = function()
+    -- Get all available colorschemes
+    local available_themes = vim.fn.getcompletion('', 'color')
+    local default_themes = {
+      'blue',
+      'darkblue',
+      'default',
+      'delek',
+      'desert',
+      'elflord',
+      'evening',
+      'industry',
+      'koehler',
+      'lunaperche',
+      'morning',
+      'murphy',
+      'pablo',
+      'peachpuff',
+      'quiet',
+      'retrobox',
+      'ron',
+      'shine',
+      'slate',
+      'torte',
+      'wildcharm',
+      'zellner',
+    }
+    -- Filter out default themes
+    local custom_themes = {}
+    for _, theme in ipairs(available_themes) do
+      local is_default = false
+      for _, default in ipairs(default_themes) do
+        if theme == default then
+          is_default = true
+          break
+        end
+      end
+      if not is_default then
+        table.insert(custom_themes, theme)
+      end
+    end
+
+    require('themery').setup {
+      themes = custom_themes,
+      livePreview = true,
+    }
+    vim.keymap.set('n', '<leader>T', '<cmd>Themery<cr>', { desc = 'Themery' })
+  end,
+}
