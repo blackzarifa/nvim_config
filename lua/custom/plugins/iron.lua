77a3bc04d760614eb7bce48b41c4cdbe034822d5
blackzarifa <rafaelirades@gmail.com>
2024-12-27 23:58:25 -0300

    refac: binds

--- lua/custom/plugins/iron.lua
+++ lua/custom/plugins/iron.lua
@@ -5,13 +5,22 @@ return {
       require('iron.core').setup {
         config = {
           repl_definition = {
-            python = { command = { 'ipython' } },
+            python = {
+              command = { 'jupyter', 'console', '--kernel', 'python3' },
+            },
           },
-          repl_open_cmd = 'botright 40split', -- Open REPL at the bottom
+          repl_open_cmd = 'belowright split',
+        },
+        require('which-key').add {
+          { '<leader>r', group = 'I[R]on' },
         },
         keymaps = {
-          send_motion = '<space>sc',
-          visual_send = '<space>sc',
+          send_motion = '<leader>rp', -- Send selected block to REPL (visual mode)
+          send_line = '<leader>rl', -- Send current line to REPL
+          send_file = '<leader>rf', -- Send the entire file to REPL
+          exit = '<leader>rq', -- Exit the REPL
+          interrupt = '<leader>ri', -- Interrupt current REPL execution
+          clear = '<leader>rc', -- Clear REPL output
         },
       }
     end,
