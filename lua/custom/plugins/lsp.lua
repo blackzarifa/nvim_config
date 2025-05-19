b0fb6beaf95f6bc94b66aa05eeb1db719f2379e0
Rafael <rafaelirades@gmail.com>
2025-05-19 14:21:33 -0300

    Revert "temp fix"
    This reverts commit 63e3ad0435b390fca33dd6443659a965199c3950.

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,19 +1,19 @@
-local vim = vim
-
 return {
+  -- LSP Configuration & Plugins
   {
     'neovim/nvim-lspconfig',
     dependencies = {
+      -- Automatically install LSPs and related tools to stdpath for neovim
       'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+      -- Useful status updates for LSP
       { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
-      -- Basic setup
-      require('mason').setup()
-
-      -- Standard keymaps
-      local on_attach = function(_, bufnr)
+      local on_attach = function(client, bufnr)
+        -- Keybinds for available LSP actions
         local map = function(keys, func, desc)
           vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
         end
@@ -21,59 +21,85 @@ return {
         map('gd', require('telescope.builtin').lsp_definitions, 'Goto Definition')
         map('gr', require('telescope.builtin').lsp_references, 'Goto References')
         map('gI', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
+
+        --  Useful when you're on a variable and want to see what type it is.
         map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type Definition')
+
+        -- Opens a popup that displays documentation about the word under your cursor
         map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+        -- Execute a code action, usually your cursor needs to be on top of an error
+        -- or warning for this to do anything.
         map('<leader>ca', vim.lsp.buf.code_action, 'Code Action')
+
+        -- Rename the variable under your cursor
         map('<leader>rn', vim.lsp.buf.rename, 'Rename')
+
         map('<leader>d', vim.diagnostic.open_float, 'Show Diagnostics')
+
+        -- Move to the previous diagnostic in your current buffer
         map('[d', vim.diagnostic.goto_prev, 'Previous Diagnostic')
+
+        -- Move to the next diagnostic in your current buffer
         map(']d', vim.diagnostic.goto_next, 'Next Diagnostic')
+
+        -- Format buffer using LSP
         map('<leader>f', function()
           vim.lsp.buf.format { async = true }
         end, 'Format')
       end
 
-      -- Capabilities
+      -- Configure LSP servers
       local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
       local lspconfig = require 'lspconfig'
 
-      -- Direct server setup - no fancy abstractions
-      lspconfig.lua_ls.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          Lua = {
-            diagnostics = { globals = { 'vim' } },
-            workspace = { checkThirdParty = false },
-          },
+      require('mason-lspconfig').setup {
+        automatic_installation = true,
+        handlers = {
+          function(server)
+            lspconfig[server].setup {
+              capabilities = capabilities,
+              on_attach = on_attach,
+            }
+          end,
         },
       }
 
-      lspconfig.volar.setup {
+      local mason_registry = require 'mason-registry'
+      local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+
+      lspconfig['ts_ls'].setup {
         capabilities = capabilities,
         on_attach = on_attach,
-        filetypes = { 'vue' },
         init_options = {
-          typescript = {
-            tsdk = vim.fn.stdpath 'data' .. '/mason/packages/typescript-language-server/node_modules/typescript/lib',
+          plugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vue_language_server_path,
+              languages = { 'javascript', 'typescript', 'vue' },
+            },
           },
+        },
+        filetypes = {
+          'javascript',
+          'typescript',
+          'vue',
         },
       }
 
-      lspconfig.ts_ls.setup {
+      lspconfig['volar'].setup {
         capabilities = capabilities,
         on_attach = on_attach,
-        filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' },
-        cmd = { 'typescript-language-server', '--stdio' },
       }
 
-      -- Configure diagnostics display
+      -- Change diagnostic symbols in the sign column
       local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
       for type, icon in pairs(signs) do
         local hl = 'DiagnosticSign' .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
       end
 
+      -- Configure diagnostics display
       vim.diagnostic.config {
         virtual_text = true,
         signs = true,
